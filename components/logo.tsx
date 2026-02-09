7a7c0f7d3561edad2dbf6e94762d08da179f1743
Swayam <158766919+Swayamjimmy@users.noreply.github.com>
2026-02-09 15:28:00 +0530

    mobile responsive

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex-1 flex items-center gap-2 text-2xl" >
-        <h1 className="font-extrabold text-primary">Fixma -</h1>
-        <p className="font-extrabold text-foreground">Design.ai</p>
+    <Link href="/" className="flex-1 flex items-center gap-2 text-lg md:text-2xl" >
+      <h1 className="font-extrabold text-primary">Fixma -</h1>
+      <p className="font-extrabold text-foreground">Design.ai</p>
     </Link>
   )
 }
